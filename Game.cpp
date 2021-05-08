280a46fbdcf7acbf22acf4495ae93f61d813b497
Brokenchin <malinen.juri.business@gmail.com>
2021-05-08 20:06:52 +0300

    Ignoring builds

---
+++ Game.cpp
@@ -0,0 +1,39 @@
+// Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <memory>
+//#include "Playground/Test_Game.h"
+//#include "Trash_Game/include/Game/Trash_Game.h"
+///* Recycling game. Perhaps 2 modes collecting trash and recycling. Could do a bit of sorts from top side with all the trash randomly put in it.. and then play can take them 1 at time to sort them. 
+//	This is game mostly for proving my skill in making a program from scratch.
+//
+//*/
+//
+//
+//
+//
+//#ifndef _DEBUG
+//#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+//#endif // __DEBUG
+//int main()
+//{
+//
+//	std::unique_ptr<Trash_Game> game = std::make_unique<Trash_Game>();
+//	game->Init(800,600);
+//    std::cout << "Hello World!\n";
+//}
+//
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
